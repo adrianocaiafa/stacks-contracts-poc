1ecda73c48da109da19c642f94641ddbb992924e
adriano.caiafa <adriano.caiafa@extreme.digital>
2025-12-21 19:18:43 -0300

    test: adiciona testes completos para wizard-game
    - Testa register-mana-transfer
    - Testa spend-mana-for-xp (conversao MANA para XP)
    - Testa atualizacao de XP e nivel
    - Testa level up quando XP atinge threshold
    - Testa cast-spell (lancar feitico)
    - Testa validacoes (nao pode lancar em si mesmo, mana insuficiente)
    - Testa metricas de interacao
    - Testa multiplos wizards

--- tests/wizard/wizard-game.test.ts
+++ tests/wizard/wizard-game.test.ts
@@ -3,6 +3,7 @@ import { describe, expect, it } from "vitest";
 const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
 const address2 = accounts.get("wallet_2")!;
+const address3 = accounts.get("wallet_3")!;
 
 describe("wizard-game tests", () => {
   it("ensures simnet is well initialised", () => {
@@ -106,5 +107,479 @@ describe("wizard-game tests", () => {
     );
     expect(result).toBeOk();
     expect(result.value.wizard-token).toBeSome(".wizard-token");
+  });
+
+  it("should allow registering mana transfer", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 2000000],
+      address2
+    );
+    
+    const { result } = simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [2000000],
+      address2
+    );
+    expect(result).toBeOk(true);
+  });
+
+  it("should update pending mana after registering", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 3000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [3000000],
+      address2
+    );
+    
+    const { result } = simnet.callReadOnlyFn(
+      "wizard-game",
+      "my-pending-mana",
+      [],
+      address2
+    );
+    expect(result).toBeUint(3000000);
+  });
+
+  it("should allow spending mana for XP", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 5000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [5000000],
+      address2
+    );
+    
+    const { result } = simnet.callPublicFn(
+      "wizard-game",
+      "spend-mana-for-xp",
+      [5000000],
+      address2
+    );
+    expect(result).toBeOk(true);
+  });
+
+  it("should update XP after spending mana", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 10000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [10000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "spend-mana-for-xp",
+      [10000000],
+      address2
+    );
+    
+    const { result } = simnet.callReadOnlyFn(
+      "wizard-game",
+      "my-xp",
+      [],
+      address2
+    );
+    expect(result).toBeUint(10);
+  });
+
+  it("should level up when XP reaches threshold", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 100000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [100000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "spend-mana-for-xp",
+      [100000000],
+      address2
+    );
+    
+    const { result: level } = simnet.callReadOnlyFn(
+      "wizard-game",
+      "my-level",
+      [],
+      address2
+    );
+    expect(level).toBeUint(1000);
+  });
+
+  it("should allow casting spell on another wizard", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 2000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [2000000],
+      address2
+    );
+    
+    const { result } = simnet.callPublicFn(
+      "wizard-game",
+      "cast-spell",
+      [address3, 2000000],
+      address2
+    );
+    expect(result).toBeOk(true);
+  });
+
+  it("should update spells cast count", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 4000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [4000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "cast-spell",
+      [address3, 2000000],
+      address2
+    );
+    
+    const { result } = simnet.callReadOnlyFn(
+      "wizard-game",
+      "my-spells-cast",
+      [],
+      address2
+    );
+    expect(result).toBeUint(1);
+  });
+
+  it("should not allow casting spell on yourself", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 2000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [2000000],
+      address2
+    );
+    
+    const { result } = simnet.callPublicFn(
+      "wizard-game",
+      "cast-spell",
+      [address2, 2000000],
+      address2
+    );
+    expect(result).toBeErr(11);
+  });
+
+  it("should not allow spending more mana than pending", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 2000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [2000000],
+      address2
+    );
+    
+    const { result } = simnet.callPublicFn(
+      "wizard-game",
+      "spend-mana-for-xp",
+      [5000000],
+      address2
+    );
+    expect(result).toBeErr(18);
+  });
+
+  it("should increment unique wizards count", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 2000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [2000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "spend-mana-for-xp",
+      [2000000],
+      address2
+    );
+    
+    const { result } = simnet.callReadOnlyFn(
+      "wizard-game",
+      "get-total-unique-wizards",
+      [],
+      address1
+    );
+    expect(result).toBeUint(1);
+  });
+
+  it("should track multiple wizards", () => {
+    simnet.callPublicFn(
+      "wizard-game",
+      "set-wizard-token",
+      [".wizard-token"],
+      address1
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "claim",
+      [],
+      address3
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 2000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-token",
+      "transfer",
+      [".wizard-game", 2000000],
+      address3
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [2000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "register-mana-transfer",
+      [2000000],
+      address3
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "spend-mana-for-xp",
+      [2000000],
+      address2
+    );
+    
+    simnet.callPublicFn(
+      "wizard-game",
+      "spend-mana-for-xp",
+      [2000000],
+      address3
+    );
+    
+    const { result } = simnet.callReadOnlyFn(
+      "wizard-game",
+      "get-total-unique-wizards",
+      [],
+      address1
+    );
+    expect(result).toBeUint(2);
   });
 });
